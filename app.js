8d876f9202ba02ac1dd6e5fca46636e396c2e928
Carlos <carlosjmpro@gmail.com>
2024-02-02 17:11:43 +0100

    visualizacion del estado de la memoria

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 'use strict'
 
+const display_top = document.getElementById('display_top');
+const display_bottom = document.getElementById('display_bottom');
+const save = document.getElementById('m+');
+const remove = document.getElementById('m-');
+const memory = 'MEMORY';
+
+save.addEventListener('click', () => {
+    console.log('memory save');
+    display_top.classList.remove('oculto');
+    //queda añadir a la memoria el contenido del display
+})
+remove.addEventListener('click', () => {
+    console.log('memory remove');
+    display_top.classList.add('oculto');
+    //queda añadir a la memoria el contenido del display
+})
