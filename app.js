fcf4ba2d75c51cb4134b3f57020e5a8a2f1fc59c
Carlos <carlosjmpro@gmail.com>
2024-02-07 11:04:41 +0100

    imprimir teclado

--- app.js
+++ app.js
@@ -4,12 +4,31 @@ const display_top = document.getElementById('display_top');
 const display_bottom = document.getElementById('display_bottom');
 const save = document.getElementById('m+');
 const remove = document.getElementById('m-');
-const number = document.getElementsByClassName('number');
-const memory = 'MEMORY';
+const number = document.querySelectorAll('.number');
+const operation = document.querySelectorAll('.op');
 
-number.addEventListener('click', () => {
-    console.log(number.textContent);
+
+
+
+number.forEach(pulsado => {
+    pulsado.addEventListener('click', () => {
+    const print = pulsado.textContent;
+    if(display_bottom.textContent === '0'){
+        display_bottom.textContent = print;
+
+    }else{
+        display_bottom.textContent += print;
+    }
+    })
+});
+
+operation.forEach(oper => {
+    oper.addEventListener('click', () => {
+        const sing = oper.textContent;
+        console.log(sing);
+    })
 })
+
 
 save.addEventListener('click', () => {
     console.log('memory save');
