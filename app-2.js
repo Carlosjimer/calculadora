5558ba269215e9abd064e8e782f77331eab0901f
Carlos <carlosjmpro@gmail.com>
2024-02-13 17:41:07 +0100

    condicionando la impresion en display

--- app-2.js
+++ app-2.js
@@ -3,7 +3,7 @@
 const display_top = document.getElementById('display_top');
 const display_bottom = document.getElementById('display_bottom');
 const button = document.querySelectorAll('button');
-let internal;
+let internal = '';
 
 
 button.forEach(buttons => {
@@ -19,28 +19,26 @@ button.forEach(buttons => {
             internal += display_bottom.textContent; 
             let internalClear = internal.slice(9);
             display_bottom.textContent = eval(internalClear);
-            return;
         }
 
         //Hacer que continue las operaciones en pantalla'
 
         if(buttons.className === "button op"){
             internal += display_bottom.textContent + print;
-            display_bottom = display_bottom.slice(-1);
             let internalClear = internal.slice(9);
-            console.log(internalClear);
+            console.log(internal);
         }
 
 
         //no funciona con mas de una operacion o con nuemros de mas de una cifra
-        if(display_bottom.textContent === '0'){
+        if(buttons.className === "button number" && display_bottom.textContent === '0' && internal.slice(-1) != '+'){
+            console.log(internal.slice(-1));
             display_bottom.textContent = print;
-        }else{
-            if(internal.slice(-1) === '+'){
-                display_bottom.textContent = print;
-            }else{
-            display_bottom.textContent += print;
-            }
+        }else if(buttons.className === "button number"){
+            if(display_bottom.textContent != '0' || internal.slice(-1) === '+'){
+            console.log(internal.slice(-1));
+                display_bottom.textContent += print;
+                }
         }
     })
 })
