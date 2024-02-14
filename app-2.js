e1b43117c12ca9506127f41274e5be7a2ac1b526
Carlos <carlosjmpro@gmail.com>
2024-02-14 21:32:12 +0100

    modificado entrada en memoria

--- app-2.js
+++ app-2.js
@@ -3,12 +3,13 @@
 const display_top = document.getElementById('display_top');
 const display_bottom = document.getElementById('display_bottom');
 const button = document.querySelectorAll('button');
-let internal = '';
+let internal;
 
 
 button.forEach(buttons => {
     buttons.addEventListener('click', () => {
         const print = buttons.textContent;
+        
 
         if(buttons.id === "c"){
             display_bottom.textContent = '0';
@@ -24,21 +25,24 @@ button.forEach(buttons => {
         //Hacer que continue las operaciones en pantalla'
 
         if(buttons.className === "button op"){
-            internal += display_bottom.textContent + print;
-            let internalClear = internal.slice(9);
-            console.log(internal);
+            internal += print;
+            console.log(internal.slice(9));
         }
 
 
         //no funciona con mas de una operacion o con nuemros de mas de una cifra
-        if(buttons.className === "button number" && display_bottom.textContent === '0' && internal.slice(-1) != '+'){
-            console.log(internal.slice(-1));
+        if(buttons.className === "button number"){
+            internal += print;
+            console.log(internal.slice(9));
+
+            if(display_bottom.textContent === '0'){
             display_bottom.textContent = print;
-        }else if(buttons.className === "button number"){
-            if(display_bottom.textContent != '0' || internal.slice(-1) === '+'){
-            console.log(internal.slice(-1));
+            }else if(buttons.className === "button number"){
+
+                if(display_bottom.textContent != '0'){
                 display_bottom.textContent += print;
                 }
+            }
         }
     })
 })
