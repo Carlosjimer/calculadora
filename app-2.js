e79a0954f7da97a7eb4b5397f4f3eaa6bbca6bf0
Carlos <carlosjmpro@gmail.com>
2024-02-21 09:34:02 +0100

    operacion sumar hecha en oculto

--- app-2.js
+++ app-2.js
@@ -6,44 +6,60 @@ const button = document.querySelectorAll('button');
 const sum = document.getElementById('sum');
 let internal;
 
-
+// quitar el guardado en internal de los numeros pulsados, solo guardar al pulsar operaciones o igual.
 button.forEach(buttons => {
     buttons.addEventListener('click', () => {
         const print = buttons.textContent;
-        
 
+        if(buttons.id === "ac"){
+            display_bottom.textContent = '0';
+            internal = '';
+            return;
+        }
+
+        // solo borrar pantalla para continuar operaciones con internal. borrar todo hasta la op en internal
         if(buttons.id === "c"){
             display_bottom.textContent = '0';
             return;
         }
 
         if(buttons.id === "equal"){
-            internal += display_bottom.textContent; 
-            let internalClear = internal.slice(9);
-            display_bottom.textContent = eval(internalClear);
+            if(internal.includes('u')){
+                let internalClear = internal.slice(9);
+                console.log(internalClear);
+                display_bottom.textContent = eval(internalClear);
+                internal = display_bottom.textContent;
+            }else{
+                display_bottom.textContent = eval(internal);
+                internal = display_bottom.textContent;
+            }
         }
 
-        //Hacer que continue las operaciones en pantalla'
+        //que solo quede la ultima operacion pulsada
 
         if(buttons.className === "button op"){
             if(buttons.id === 'sum'){
                 internal += '+';
+            }else if(buttons.id === 'subtr'){
+                internal += '-';
+            }else if(buttons.id === 'multi'){
+                internal += '*';
+            }else if(buttons.id === 'divi'){
+                internal += '/';
             }
             console.log(internal.slice(9), internal.slice(-1));
         }
 
 
-        //no funciona con mas de una operacion o con nuemros de mas de una cifra
         if(buttons.className === "button number"){
             
-            //console.log(internal.slice(9), typeof(internal.slice(-1)));
-            
-            if(display_bottom.textContent === '0' || typeof(internal.slice(-1)) != 'string'){
+            if(display_bottom.textContent === '0' || internal.slice(-1) === '+' || internal.slice(-1) === '-' || internal.slice(-1) === 'x' || internal.slice(-1) === '+'){
                 display_bottom.textContent = print;
             }else{
                 display_bottom.textContent += print;
             }
             internal += print;
+            console.log(internal.slice(9));
         }
     })
 })
