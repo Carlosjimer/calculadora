6e78138bab280bcb7327eddbbe14d334fd5b1462
Carlos <carlosjmpro@gmail.com>
2024-02-22 13:38:39 +0100

    Logica de operaciones despues de realizar eval

--- app-2.js
+++ app-2.js
@@ -4,7 +4,7 @@ const display_top = document.getElementById('display_top');
 const display_bottom = document.getElementById('display_bottom');
 const button = document.querySelectorAll('button');
 const sum = document.getElementById('sum');
-let internal;
+let internal ='';
 
 // quitar el guardado en internal de los numeros pulsados, solo guardar al pulsar operaciones o igual.
 button.forEach(buttons => {
@@ -24,32 +24,46 @@ button.forEach(buttons => {
         }
 
         if(buttons.id === "equal"){
-            internal += display_bottom.textContent;
-            if(internal.includes('u')){
+            if(internal === ''){
+                /*(internal.includes('u')){
                 let internalClear = internal.slice(9);
                 console.log(internalClear);
                 display_bottom.textContent = eval(internalClear);
-                internal = display_bottom.textContent;
+                internal = display_bottom.textContent;*/
             }else{
                 display_bottom.textContent = eval(internal);
                 internal = display_bottom.textContent;
             }
+            internal = '';
+            console.log(internal);
         }
 
-        //que solo quede la ultima operacion pulsada
+        //que solo quede la ultima operacion pulsada y que despues de pulsar igual podamos seguir operando.
 
         if(buttons.className === "button op"){
-            internal += display_bottom.textContent;
-            if(buttons.id === 'sum'){
-                internal += '+';
-            }else if(buttons.id === 'subtr'){
-                internal += '-';
-            }else if(buttons.id === 'multi'){
-                internal += '*';
-            }else if(buttons.id === 'divi'){
-                internal += '/';
+            if(internal === ''){
+                internal = display_bottom;
+                if(buttons.id === 'sum'){
+                    internal += '+';
+                }else if(buttons.id === 'subtr'){
+                    internal += '-';
+                }else if(buttons.id === 'multi'){
+                    internal += '*';
+                }else if(buttons.id === 'divi'){
+                    internal += '/';
+                }
+            }else{
+                if(buttons.id === 'sum'){
+                    internal += '+';
+                }else if(buttons.id === 'subtr'){
+                    internal += '-';
+                }else if(buttons.id === 'multi'){
+                    internal += '*';
+                }else if(buttons.id === 'divi'){
+                    internal += '/';
+                }
+            console.log(internal)//.slice(9));
             }
-            console.log(internal.slice(9));
         }
 
 
@@ -57,9 +71,12 @@ button.forEach(buttons => {
             
             if(display_bottom.textContent === '0' || internal.slice(-1) === '+' || internal.slice(-1) === '-' || internal.slice(-1) === 'x' || internal.slice(-1) === '/'){
                 display_bottom.textContent = print;
+                internal += print;
             }else{
                 display_bottom.textContent += print;
+                internal += print;
             }
+            console.log(internal);
         }
     })
 })
