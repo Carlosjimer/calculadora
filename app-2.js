a7611e1c3def2943c6ea95aab7e24bceeb631b23
Carlos <carlosjmpro@gmail.com>
2024-02-27 10:58:50 +0100

    Mejorado el comportamiento del boton C y puesto en funcionamiento la memoria. Falta que aparezca el estado activo de la memoria en pantalla.

--- app-2.js
+++ app-2.js
@@ -4,22 +4,45 @@ const display_top = document.getElementById('display_top');
 const display_bottom = document.getElementById('display_bottom');
 const button = document.querySelectorAll('button');
 const sum = document.getElementById('sum');
+
+//Variante que se utilizará para realizar las operaciones sin que aparezcan en pantalla.
 let internal ='';
 
+
+//Se recorren todos los botones agregando el evento click para su posterior ejecución.
 button.forEach(buttons => {
     buttons.addEventListener('click', () => {
         const print = buttons.textContent;
 
+//El boton OFF apaga la pantalla y reinicia la constante internal.
         if(buttons.id === 'off'){
             display_bottom.textContent = ''
             internal = '0';
         }
 
-        if(buttons.id === 'm+'){}
+/*
+El boton M+ guarda en localStorage el contenido del display para que continue presente aun apando la calculadora.
+Además modificara la clase en CSS del elemento display_top para que aparezca cuando tengamos algún numero guardado.
+*/
+        if(buttons.id === 'm+'){
+            localStorage.setItem('mem', display_bottom.textContent);
+            console.log(localStorage.getItem('mem'));
+        }
 
-        if(buttons.id === 'm-'){}
+        if(buttons.id === 'm-'){
+            localStorage.clear();
+            display_bottom.textContent = 0;
+        }
 
-        if(buttons.id === 'mrc'){}
+        if(buttons.id === 'mrc'){
+            if(localStorage.getItem('mem') === null){
+                display_bottom.textContent = 0;
+            }else{
+                display_bottom.textContent = localStorage.getItem('mem');
+                internal += display_bottom.textContent;
+                console.log(localStorage.getItem('mem'));
+            }
+        }
 
 
         if(buttons.id === "ac"){
@@ -29,7 +52,9 @@ button.forEach(buttons => {
         }
 
         if(buttons.id === "c"){
+            internal = internal.slice(0, - display_bottom.textContent.length);
             display_bottom.textContent = '0';
+            console.log(internal);
             return;
         }
 
